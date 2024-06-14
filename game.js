e13fadca5f41959a008ecb790ab21be791a6f148
RohitBhojak <rohitbhojak99@gmail.com>
2024-06-14 22:37:28 +0530

    Add game-over and score-board

--- game.js
+++ game.js
@@ -8,17 +8,20 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    LOSE = () => {
-        score.textContent = "YOU LOSE";
-        score.style.color = "red";
+    function LOSE () {
+        counter.textContent = "YOU LOSE";
+        counter.style.color = "red";
         computerScore++;
     }
-    WIN =  () => {
-        score.textContent = "YOU WIN";
-        score.style.color = "green";
+    function WIN () {
+        counter.textContent = "YOU WIN";
+        counter.style.color = "green";
         humanScore++;
     }
-    TIE =  () => score.textContent = "TIE!";;
+    function TIE() {
+        counter.textContent = "TIE!";
+        counter.style.color = "black";
+    }
 
     if (humanChoice == "rock") {
         switch (computerChoice) {
@@ -45,12 +48,30 @@ function playRound(humanChoice, computerChoice) {
             case "paper" : WIN();
         }
     }
+    userScore.textContent = humanScore;
+    cpuScore.textContent = computerScore;
+    if (humanScore == 5 || computerScore == 5) {
+        endgame(humanScore, computerScore)
+    }
 }
 
 let buttons = document.querySelectorAll("button");
 buttons = Array.from(buttons);
 let computerScore = 0, humanScore = 0;
-score = document.querySelector(".counter");
+let counter = document.querySelector(".counter");
+let cpuScore = document.querySelector(".cpuScore");
+let userScore = document.querySelector(".userScore");
 buttons.forEach((button) => {
     button.addEventListener('click', () => playRound(button.id, getComputerChoice()));
 })
+
+function endgame(userScore, cpuScore) {
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
+    let gameOver = document.querySelector(".game-over");
+    gameOver.textContent = "GAME OVER!";
+    gameOver.classList.toggle("active");
+    if (cpuScore == 5) gameOver.style.backgroundColor = "red";
+    else gameOver.style.backgroundColor = "green";
+}
